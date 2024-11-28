f00812838503641b44444fc9a001648ac6b26acc
hypershwegyi <shweyeetheint690@gmail.com>
2024-11-28 20:13:49 +0630

    javascript,index.html,Readme.md

---
+++ 03-JavaScript/01-Activities/02-Iteration/script.js
@@ -0,0 +1,5 @@
+var students = ['WaiWai','YinYin','TunTun','PanPan','OoOo']
+console.log(students.length);
+for (var i = 0; i < students.length; i++){
+    console.log ('Great to see you,' + students[i] + '!');
+}
